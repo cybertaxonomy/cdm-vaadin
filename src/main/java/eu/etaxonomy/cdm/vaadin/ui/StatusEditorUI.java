c6974750282e3f69b8cdeeb072c594c9012f9ffb
Cherian Mathew <c.mathew@bgbm.org>
2015-03-12 15:14:37 +0000

    StatusComposite : added ui elements for filtering add / removing taxa styles.css : added custom styles for filter table , grid spacing StatusEditorUI : changed theme to mytheme

--- src/main/java/eu/etaxonomy/cdm/vaadin/ui/StatusEditorUI.java
+++ src/main/java/eu/etaxonomy/cdm/vaadin/ui/StatusEditorUI.java
@@ -29,7 +29,7 @@ import com.vaadin.ui.UI;
 import eu.etaxonomy.cdm.vaadin.view.StatusEditorView;
 
 
-@Theme("chameleon")
+@Theme("mytheme")
 public class StatusEditorUI extends AbstractAuthenticatedUI {
 
     Navigator navigator;
