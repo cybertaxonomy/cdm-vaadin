e693adbd5825e12222d3150b5dd296de3fcdca88
Patrick Plitzner <p.plitzner@bgbm.org>
2016-10-11 15:18:16 +0200

    ref #5458 Reduce size of distribution table
     - Show only abbreviated labels for presence absence terms

--- src/main/java/eu/etaxonomy/cdm/vaadin/container/PresenceAbsenceTermContainer.java
+++ src/main/java/eu/etaxonomy/cdm/vaadin/container/PresenceAbsenceTermContainer.java
@@ -2,14 +2,15 @@ package eu.etaxonomy.cdm.vaadin.container;
 
 import java.util.List;
 
-import com.vaadin.data.util.BeanItemContainer;
+import com.vaadin.data.util.BeanContainer;
 
+import eu.etaxonomy.cdm.model.common.Language;
 import eu.etaxonomy.cdm.model.common.TermType;
 import eu.etaxonomy.cdm.model.description.PresenceAbsenceTerm;
 import eu.etaxonomy.cdm.vaadin.util.CdmSpringContextHelper;
 import eu.etaxonomy.cdm.vaadin.util.TermCacher;
 
-public class PresenceAbsenceTermContainer extends BeanItemContainer<PresenceAbsenceTerm> {
+public class PresenceAbsenceTermContainer extends BeanContainer<String, PresenceAbsenceTerm> {
 
 	private static final long serialVersionUID = -7891310979870159325L;
 
@@ -18,6 +19,15 @@ public class PresenceAbsenceTermContainer extends BeanItemContainer<PresenceAbse
 	private PresenceAbsenceTermContainer()
 			throws IllegalArgumentException {
 		super(PresenceAbsenceTerm.class);
+		setBeanIdResolver(new BeanIdResolver<String, PresenceAbsenceTerm>() {
+
+            private static final long serialVersionUID = -6008191522128487319L;
+
+            @Override
+            public String getIdForBean(PresenceAbsenceTerm bean) {
+                return bean.getRepresentation(Language.DEFAULT()).getAbbreviatedLabel();
+            }
+        });
 		List<PresenceAbsenceTerm> terms = CdmSpringContextHelper.getTermService().listByTermType(TermType.PresenceAbsenceTerm, null, null, null, null);
 		TermCacher termCacher = TermCacher.getInstance();
 		for (PresenceAbsenceTerm presenceAbsenceTerm : terms) {
