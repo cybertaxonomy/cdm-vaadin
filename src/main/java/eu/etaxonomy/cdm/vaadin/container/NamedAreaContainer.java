44123d653cf0caaa2dbcbab74e06225e440da036
Patrick Plitzner <p.plitzner@bgbm.org>
2016-10-26 16:01:30 +0200

    ref #5458 Sort named areas

--- src/main/java/eu/etaxonomy/cdm/vaadin/container/NamedAreaContainer.java
+++ src/main/java/eu/etaxonomy/cdm/vaadin/container/NamedAreaContainer.java
@@ -1,6 +1,9 @@
 package eu.etaxonomy.cdm.vaadin.container;
 
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 import com.vaadin.data.util.BeanItemContainer;
 
@@ -15,12 +18,22 @@ public class NamedAreaContainer extends BeanItemContainer<NamedArea> {
 	public NamedAreaContainer(TermVocabulary<NamedArea> vocabulary)
 	        throws IllegalArgumentException {
 	    super(NamedArea.class);
-	    Set<NamedArea> namedAreas = vocabulary.getTerms();
+	    List<NamedArea> namedAreas = new ArrayList<>(vocabulary.getTerms());
+	    Collections.sort(namedAreas, new AlphabeticallyAscendingNamedAreaComparator());
 	    TermCacher termCacher = TermCacher.getInstance();
 	    for (NamedArea namedArea: namedAreas) {
 	        termCacher.addNamedArea(namedArea);
 	        addBean(namedArea);
 	    }
+	}
+
+	private class AlphabeticallyAscendingNamedAreaComparator implements Comparator<NamedArea>{
+
+        @Override
+        public int compare(NamedArea o1, NamedArea o2) {
+            return o2.compareTo(o1);
+        }
+
 	}
 
 }
