c850e7a95def10422868dc3c5948d6b7ed7deae7
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2017-02-27 11:29:36 +0100

    attempt to create a responsive layout with menu

--- src/main/java/eu/etaxonomy/cdm/vaadin/themes/valo/ValoMenuLayout.java
+++ src/main/java/eu/etaxonomy/cdm/vaadin/themes/valo/ValoMenuLayout.java
@@ -8,6 +8,10 @@
 */
 package eu.etaxonomy.cdm.vaadin.themes.valo;
 
+import com.vaadin.server.FontAwesome;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.ComponentContainer;
 import com.vaadin.ui.CssLayout;
@@ -47,6 +51,27 @@ public class ValoMenuLayout extends HorizontalLayout {
     public void addMenu(Component menu) {
         menu.addStyleName(ValoTheme.MENU_PART);
         menuArea.addComponent(menu);
+    }
+
+    protected void init() {
+
+        final CssLayout menu = new CssLayout();
+        Button showMenu = new Button("Menu", new ClickListener() {
+            @Override
+            public void buttonClick(ClickEvent event) {
+                if (menu.getStyleName().contains("valo-menu-visible")) {
+                    menu.removeStyleName("valo-menu-visible");
+                } else {
+                    menu.addStyleName("valo-menu-visible");
+                }
+            }
+        });
+        showMenu.addStyleName(ValoTheme.BUTTON_PRIMARY);
+        showMenu.addStyleName(ValoTheme.BUTTON_SMALL);
+        showMenu.addStyleName("valo-menu-toggle");
+        showMenu.setIcon(FontAwesome.LIST);
+        menu.addComponent(showMenu);
+
     }
 
 }
