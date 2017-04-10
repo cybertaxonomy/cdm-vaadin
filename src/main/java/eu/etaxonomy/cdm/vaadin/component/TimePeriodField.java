5367d5aa4da6ec9f58a8602fef80e197c96f6d1d
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2017-04-10 17:03:38 +0200

    ref #6169 TimePeriod field with simple and details view

--- src/main/java/eu/etaxonomy/cdm/vaadin/component/TimePeriodField.java
+++ src/main/java/eu/etaxonomy/cdm/vaadin/component/TimePeriodField.java
@@ -11,13 +11,16 @@ package eu.etaxonomy.cdm.vaadin.component;
 import com.vaadin.data.fieldgroup.BeanFieldGroup;
 import com.vaadin.data.util.BeanItem;
 import com.vaadin.event.FieldEvents.TextChangeEvent;
+import com.vaadin.server.FontAwesome;
 import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Button;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.CssLayout;
 import com.vaadin.ui.CustomField;
 import com.vaadin.ui.GridLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.TextField;
+import com.vaadin.ui.themes.ValoTheme;
 
 import eu.etaxonomy.cdm.model.common.TimePeriod;
 import eu.etaxonomy.cdm.strategy.parser.TimePeriodParser;
@@ -44,6 +47,13 @@ public class TimePeriodField extends CustomField<TimePeriod> {
 
     GridLayout grid = new GridLayout(3, 3);
 
+    CssLayout detailsView = new CssLayout();
+    CssLayout buttonTextField = new CssLayout();
+
+    CssLayout simpleView = new CssLayout();
+
+    TextField cacheField = new TextField();
+
     /**
      *
      */
@@ -67,10 +77,49 @@ public class TimePeriodField extends CustomField<TimePeriod> {
 
         super.setPrimaryStyleName(PRIMARY_STYLE);
 
+        CssLayout root = new CssLayout();
+
+        initSimpleView();
+        root.addComponent(simpleView);
+
+        initDetailsView();
+        root.addComponent(detailsView);
+
+        applyDefaultStyles();
+
+        showSimple();
+
+        return root;
+    }
+
+    /**
+     *
+     */
+    private void initSimpleView() {
+
+        cacheField.setWidth(100, Unit.PERCENTAGE);
+        Button showDetailsButton = new Button(FontAwesome.CALENDAR);
+        showDetailsButton.addClickListener(e -> showDetails());
+        simpleView.addComponent(showDetailsButton);
+        simpleView.addComponent(cacheField);
+        simpleView.addStyleName(ValoTheme.LAYOUT_COMPONENT_GROUP);
+    }
+
+    /**
+     *
+     */
+    private void initDetailsView() {
+
         parseField = new TextField();
         parseField.setWidth(100, Unit.PERCENTAGE);
         parseField.setInputPrompt("This field will parse the entered time period");
         parseField.addTextChangeListener(e -> parseInput(e));
+
+        Button closeDetailsButton = new Button(FontAwesome.CLOSE);
+        closeDetailsButton.addClickListener(e -> showSimple());
+
+        buttonTextField.addComponent(closeDetailsButton);
+        buttonTextField.addComponent(parseField);
 
         PartialDateField startDate = new PartialDateField("Start");
         startDate.setInputPrompt("dd.mm.yyy");
@@ -86,7 +135,7 @@ public class TimePeriodField extends CustomField<TimePeriod> {
         toLabel = new Label("\u2014"); // EM DASH : 0x2014
 
         int row = 0;
-        grid.addComponent(parseField, 0, row, 2, row);
+        grid.addComponent(buttonTextField, 0, row, 2, row);
         row++;
         grid.addComponent(startDate, 0, row);
         grid.addComponent(toLabel, 1, row);
@@ -97,16 +146,26 @@ public class TimePeriodField extends CustomField<TimePeriod> {
 
         grid.iterator().forEachRemaining(c -> c.setStyleName(getStyleName()));
 
-        CssLayout marginwrapper = new CssLayout();
-        marginwrapper.setStyleName("margin-wrapper");
-        marginwrapper.addComponent(grid);
-
-        applyDefaultStyles();
-
-        return marginwrapper;
+        detailsView.setStyleName("margin-wrapper");
+        detailsView.addComponent(grid);
     }
 
 
+    /**
+     * @return
+     */
+    private void showSimple() {
+        detailsView.setVisible(false);
+        simpleView.setVisible(true);
+    }
+
+    /**
+     * @return
+     */
+    private void showDetails() {
+        detailsView.setVisible(true);
+        simpleView.setVisible(false);
+    }
 
     /**
      * @param e
@@ -126,6 +185,7 @@ public class TimePeriodField extends CustomField<TimePeriod> {
         if(parseField != null) {
             parseField.addStyleName(RegistrationStyles.HELPER_FIELD);
             toLabel.addStyleName("to-label");
+            buttonTextField.addStyleName(ValoTheme.LAYOUT_COMPONENT_GROUP);
         }
 
     }
@@ -134,12 +194,18 @@ public class TimePeriodField extends CustomField<TimePeriod> {
     protected void setInternalValue(TimePeriod newValue) {
         super.setInternalValue(newValue);
         fieldGroup.setItemDataSource(new BeanItem<TimePeriod>(newValue));
+
+        cacheField.setReadOnly(false);
+        cacheField.setValue(newValue.toString());
+        cacheField.setReadOnly(true);
     }
 
     @Override
     public void setStyleName(String style) {
         super.setStyleName(style);
         grid.iterator().forEachRemaining(c -> c.setStyleName(style));
+        buttonTextField.iterator().forEachRemaining(c -> c.setStyleName(style));
+        simpleView.iterator().forEachRemaining(c -> c.setStyleName(style));
         applyDefaultStyles();
     }
 
@@ -147,6 +213,8 @@ public class TimePeriodField extends CustomField<TimePeriod> {
     public void addStyleName(String style) {
         super.addStyleName(style);
         grid.iterator().forEachRemaining(c -> c.addStyleName(style));
+        simpleView.iterator().forEachRemaining(c -> c.addStyleName(style));
+        buttonTextField.iterator().forEachRemaining(c -> c.addStyleName(style));
     }
 
     /**
