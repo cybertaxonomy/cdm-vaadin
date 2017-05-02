62b8daac3eef1489d2c3576dd7619de86f0b355a
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2017-05-02 15:11:37 +0200

    auto login feature for developers

--- src/main/java/eu/etaxonomy/cdm/vaadin/view/LoginPresenter.java
+++ src/main/java/eu/etaxonomy/cdm/vaadin/view/LoginPresenter.java
@@ -8,6 +8,8 @@
 */
 package eu.etaxonomy.cdm.vaadin.view;
 
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.event.EventListener;
@@ -30,6 +32,13 @@ import eu.etaxonomy.vaadin.ui.navigation.NavigationManager;
  * The {@link LoginView is used as replacement view in the scope of other views.
  * Therefore the LoginPresenter must be in <b>UIScope</b> so that the LoginPresenter
  * is available to all Views.
+ * <p>
+ * The LoginPresenter offers a <b>auto login feature for developers</b>. Tio activate the auto login
+ * you need to provide the <code>user name</code> and <code>password</code> using the environment variables
+ * <code>cdm-vaadin.login.usr</code> and <code>cdm-vaadin.login.pwd</code>, e.g.:
+ * <pre>
+ * -Dcdm-vaadin.login.usr=admin -Dcdm-vaadin.login.pwd=00000
+ * </pre>
  *
  * @author a.kohlbecker
  * @since Apr 25, 2017
@@ -40,6 +49,12 @@ import eu.etaxonomy.vaadin.ui.navigation.NavigationManager;
 public class LoginPresenter extends AbstractPresenter<LoginView> {
 
     private static final long serialVersionUID = 4020699735656994791L;
+
+    private static final Logger log = Logger.getLogger(LoginPresenter.class);
+
+    private final static String PROPNAME_USER = "cdm-vaadin.login.usr";
+
+    private final static String PROPNAME_PASSWORD = "cdm-vaadin.login.pwd";
 
     @Autowired
     protected ApplicationEventPublisher eventBus;
@@ -60,8 +75,10 @@ public class LoginPresenter extends AbstractPresenter<LoginView> {
         Authentication authentication = authenticationManager.authenticate(token);
 
         if(authentication != null && authentication.isAuthenticated()) {
+            log.debug("user '" + userName + "' autheticated");
             currentSecurityContext().setAuthentication(authentication);
             if(NavigationManager.class.isAssignableFrom(getNavigationManager().getClass())){
+                log.debug("reloading current view");
                 getNavigationManager().reloadCurrentView();
                 eventBus.publishEvent(new AuthenticationSuccessEvent(userName));
             }
@@ -69,6 +86,20 @@ public class LoginPresenter extends AbstractPresenter<LoginView> {
         return false;
     }
 
+
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void onViewEnter() {
+        super.onViewEnter();
+        // attempt to auto login
+        if(StringUtils.isNotEmpty(System.getProperty(PROPNAME_USER)) && StringUtils.isNotEmpty(System.getProperty(PROPNAME_PASSWORD))){
+            log.warn("Performing autologin with user " + System.getProperty(PROPNAME_USER));
+            authenticate(System.getProperty(PROPNAME_USER), System.getProperty(PROPNAME_PASSWORD));
+        }
+    }
 
     @EventListener
     protected void onLoginEvent(AuthenticationAttemptEvent e){
