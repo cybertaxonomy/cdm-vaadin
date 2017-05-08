10f9654c14ed8f93e981b79c0188d50034e19378
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2017-05-08 21:50:05 +0200

    fix #6628 UI enablement via per instance property files, UIs are now inactive per default

--- src/main/java/eu/etaxonomy/cdm/addon/config/CdmVaadinConfiguration.java
+++ src/main/java/eu/etaxonomy/cdm/addon/config/CdmVaadinConfiguration.java
@@ -8,16 +8,35 @@
 */
 package eu.etaxonomy.cdm.addon.config;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Properties;
+
 import javax.servlet.annotation.WebServlet;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.Lazy;
 
 import com.vaadin.spring.annotation.EnableVaadin;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.spring.annotation.UIScope;
 import com.vaadin.spring.server.SpringVaadinServlet;
+import com.vaadin.ui.UI;
 
+import eu.etaxonomy.cdm.opt.config.DataSourceConfigurer;
 import eu.etaxonomy.cdm.vaadin.security.annotation.EnableAnnotationBasedAccessControl;
+import eu.etaxonomy.cdm.vaadin.ui.ConceptRelationshipUI;
+import eu.etaxonomy.cdm.vaadin.ui.DistributionStatusUI;
+import eu.etaxonomy.cdm.vaadin.ui.InactiveUIException;
+import eu.etaxonomy.cdm.vaadin.ui.RegistrationUI;
+import eu.etaxonomy.cdm.vaadin.ui.StatusEditorUI;
+import eu.etaxonomy.cdm.vaadin.util.ConfigFileUtils;
 import eu.etaxonomy.vaadin.ui.annotation.EnableVaadinSpringNavigation;
 
 /**
@@ -31,13 +50,29 @@ import eu.etaxonomy.vaadin.ui.annotation.EnableVaadinSpringNavigation;
         "eu.etaxonomy.cdm.vaadin",
         "eu.etaxonomy.vaadin.ui",
         "eu.etaxonomy.cdm.mock" // FIXME remove once mocks are no longer needed
-        })
+        },
+        // exclude UI classes, these are provided via the @Bean annotated methods below
+        excludeFilters={@Filter(
+                pattern="eu\\.etaxonomy\\.cdm\\.vaadin\\.ui\\..*",
+                type=FilterType.REGEX
+                )
+            })
 @EnableVaadin   // this imports VaadinConfiguration
 @EnableVaadinSpringNavigation // activate the NavigationManagerBean
 @EnableAnnotationBasedAccessControl // enable annotation based per view access control
 public class CdmVaadinConfiguration {
 
+    /**
+     *
+     */
+    private static final String CDM_VAADIN_UI_ACTIVATED = "cdm-vaadin.ui.activated";
+
     public static final Logger logger = Logger.getLogger(CdmVaadinConfiguration.class);
+
+    @Autowired
+    @Lazy
+    //FIXME consider to set the instanceName (instanceID) in the spring environment to avoid a bean reference here
+    private DataSourceConfigurer dataSourceConfigurer;
 
     /*
      * NOTE: It is necessary to map the URLs starting with /VAADIN/* since none of the
@@ -46,10 +81,102 @@ public class CdmVaadinConfiguration {
      */
     @WebServlet(value = {"/app/*", "/VAADIN/*"}, asyncSupported = true)
     public static class Servlet extends SpringVaadinServlet {
+
+        private static final long serialVersionUID = -2615042297393028775L;
+
+        /**
+         *
+        @SuppressWarnings("serial")
+        @Override
+        protected void servletInitialized() throws ServletException {
+            getService().addSessionInitListener(new SessionInitListener() {
+
+                @Override
+                public void sessionInit(SessionInitEvent sessionInitEvent) throws ServiceException {
+                    VaadinSession session = sessionInitEvent.getSession();
+                    session.setErrorHandler(new DefaultErrorHandler(){
+
+                        @Override
+                        public void error(ErrorEvent errorEvent) {
+                            if(errorEvent.getThrowable() instanceof InactiveUIException){
+                                //TODO redirect to an ErrorUI or show and error Page
+                                // better use Spring MVC Error handlers instead?
+                            } else {
+                                doDefault(errorEvent);
+                            }
+                        }
+
+                    });
+
+                }});
+
+        }
+         */
+
     }
 
     public CdmVaadinConfiguration() {
         logger.debug("CdmVaadinConfiguration enabled");
+    }
+
+    @Bean
+    @UIScope
+    public ConceptRelationshipUI conceptRelationshipUI() throws InactiveUIException {
+        if(isUIEnabled(ConceptRelationshipUI.class)){
+            return new ConceptRelationshipUI();
+        }
+        return null;
+    }
+
+    @Bean
+    @UIScope
+    public RegistrationUI registrationUI() throws InactiveUIException {
+        if(isUIEnabled(RegistrationUI.class)){
+            return new RegistrationUI();
+        }
+        return null;
+    }
+
+    @Bean
+    @UIScope
+    public DistributionStatusUI distributionStatusUI() throws InactiveUIException {
+        if(isUIEnabled(DistributionStatusUI.class)){
+            return new DistributionStatusUI();
+        }
+        return null;
+    }
+
+    @Bean
+    @UIScope
+    public StatusEditorUI statusEditorUI() throws InactiveUIException {
+        if(isUIEnabled(StatusEditorUI.class)){
+            return new StatusEditorUI();
+        }
+        return null;
+    }
+
+
+    /**
+     * Checks if the ui class supplied is activated by listing it in the properties by its {@link SpringUI#path()} value.
+     *
+     * @param type
+     * @return
+     * @throws InactiveUIException
+     */
+    private boolean isUIEnabled(Class<? extends UI>uiClass) throws InactiveUIException {
+        String path = uiClass.getAnnotation(SpringUI.class).path();
+
+        try {
+            Properties appProps = ConfigFileUtils.getApplicationProperties(dataSourceConfigurer.dataSourceProperties().getCurrentDataSourceId());
+            if(appProps.get(CDM_VAADIN_UI_ACTIVATED) != null){
+                String[] uiPaths = appProps.get(CDM_VAADIN_UI_ACTIVATED).toString().split("\\s*,\\s*");
+                return Arrays.asList(uiPaths).stream().anyMatch(p -> p.equals(path));
+            }
+            throw new InactiveUIException(path);
+        } catch (IOException e) {
+            logger.error("Error reading the vaadin ui properties file. File corrupted?. Stopping instance ...");
+            throw new RuntimeException(e);
+        }
     }
 
 
---
+++ src/main/java/eu/etaxonomy/cdm/vaadin/util/ConfigFileUtils.java
@@ -0,0 +1,52 @@
+/**
+* Copyright (C) 2017 EDIT
+* European Distributed Institute of Taxonomy
+* http://www.e-taxonomy.eu
+*
+* The contents of this file are subject to the Mozilla Public License Version 1.1
+* See LICENSE.TXT at the top of this package for the full license terms.
+*/
+package eu.etaxonomy.cdm.vaadin.util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import eu.etaxonomy.cdm.common.CdmUtils;
+
+/**
+ * @author a.kohlbecker
+ * @since May 8, 2017
+ *
+ */
+public class ConfigFileUtils extends CdmUtils {
+
+    private static final String CDM_VAADIN_CONFIG_FOLDER = "cdm-vaadin";
+
+    //TODO better store in VaadinSession?
+    static Properties uiprops = null;
+
+    public static File getPropertiesFile(String instanceName, String propertiesSet) {
+
+        File vaadinConfigFolder = getCdmSubDir(CDM_VAADIN_CONFIG_FOLDER);
+        return new File(vaadinConfigFolder, instanceName + (propertiesSet == null? "" : "-" + propertiesSet) + ".properties");
+
+    }
+
+    public static Properties getApplicationProperties(String instanceName) throws IOException {
+        if(uiprops == null){
+            uiprops = new Properties();
+            File uiPropertiesFile = getPropertiesFile(instanceName, "app");
+            if(uiPropertiesFile.exists()){
+                try {
+                    uiprops.load(new FileInputStream(uiPropertiesFile));
+                } catch (FileNotFoundException e) {
+                    // must not happen since we checked before
+                }
+            }
+        }
+        return uiprops;
+    }
+}
