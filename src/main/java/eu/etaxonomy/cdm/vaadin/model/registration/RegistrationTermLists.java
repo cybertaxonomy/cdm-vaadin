253be0c861ced46c84c53179f0423c7d3b5c6a47
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2017-06-26 09:40:50 +0200

    ref #6193 adding more terms

--- src/main/java/eu/etaxonomy/cdm/vaadin/model/registration/RegistrationTermLists.java
+++ src/main/java/eu/etaxonomy/cdm/vaadin/model/registration/RegistrationTermLists.java
@@ -13,6 +13,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
+import eu.etaxonomy.cdm.model.name.NameTypeDesignationStatus;
 import eu.etaxonomy.cdm.model.name.SpecimenTypeDesignationStatus;
 import eu.etaxonomy.cdm.model.occurrence.DerivationEventType;
 /**
@@ -30,16 +31,34 @@ public class RegistrationTermLists {
         });
 
     public static final List<SpecimenTypeDesignationStatus> SPECIMEN_TYPE_DESIGNATION_STATUS = Arrays.asList(new SpecimenTypeDesignationStatus[]{
-            SpecimenTypeDesignationStatus.SYNTYPE(),
-            SpecimenTypeDesignationStatus.TYPE(),
+            SpecimenTypeDesignationStatus.EPITYPE(),
             SpecimenTypeDesignationStatus.HOLOTYPE(),
+            SpecimenTypeDesignationStatus.ISOEPITYPE(),
+            SpecimenTypeDesignationStatus.ISOLECTOTYPE(),
+            SpecimenTypeDesignationStatus.ISONEOTYPE(),
+            SpecimenTypeDesignationStatus.ISOPARATYPE(),
+            SpecimenTypeDesignationStatus.ISOSYNTYPE(),
+            SpecimenTypeDesignationStatus.ISOTYPE(),
             SpecimenTypeDesignationStatus.LECTOTYPE(),
-            SpecimenTypeDesignationStatus.SECOND_STEP_LECTOTYPE(),
             SpecimenTypeDesignationStatus.NEOTYPE(),
-            SpecimenTypeDesignationStatus.EPITYPE()
+            SpecimenTypeDesignationStatus.PARALECTOTYPE(),
+            SpecimenTypeDesignationStatus.PARANEOTYPE(),
+            SpecimenTypeDesignationStatus.PARATYPE(),
+            SpecimenTypeDesignationStatus.SECOND_STEP_LECTOTYPE(),
+            SpecimenTypeDesignationStatus.SECOND_STEP_NEOTYPE(),
+            SpecimenTypeDesignationStatus.SYNTYPE(),
+            SpecimenTypeDesignationStatus.TYPE(), // Unknown type category
+            SpecimenTypeDesignationStatus.UNSPECIFIC(),
+
             // TODO add Typus consservandus
             // TODO add Typ. cons. prop.
     });
+
+    public static final List<NameTypeDesignationStatus> NAME_TYPE_DESIGNATION_STATUS = Arrays.asList(new NameTypeDesignationStatus[]{
+            NameTypeDesignationStatus.LECTOTYPE()
+            // TODO add morem see https://dev.e-taxonomy.eu/redmine/issues/6193
+    });
+
 
 
 
@@ -52,6 +71,11 @@ public class RegistrationTermLists {
     public static List<UUID> SPECIMEN_TYPE_DESIGNATION_STATUS_UUIDS(){
         List<UUID> uuids = new ArrayList<>();
         SPECIMEN_TYPE_DESIGNATION_STATUS.forEach(t -> uuids.add(t.getUuid()));
+        return uuids;
+    }
+    public static List<UUID> NAME_TYPE_DESIGNATION_STATUS_UUIDS(){
+        List<UUID> uuids = new ArrayList<>();
+        NAME_TYPE_DESIGNATION_STATUS.forEach(t -> uuids.add(t.getUuid()));
         return uuids;
     }
 
