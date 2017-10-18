60be1b8d17d7762a1c794f407905ceeecca41b4f
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2017-10-18 15:11:33 +0200

    comment on saveOrUpdate operation

--- src/main/java/eu/etaxonomy/cdm/service/CdmStore.java
+++ src/main/java/eu/etaxonomy/cdm/service/CdmStore.java
@@ -200,8 +200,10 @@ public class CdmStore<T extends CdmBase, S extends IService<T>> {
 
         logger.trace(this._toString() + ".onEditorSaveEvent - merging bean into session");
         // merge the changes into the session, ...
+
         T mergedBean = mergedBean(bean);
         //T mergedBean = bean;
+        // NOTE: saveOrUpdate is really needed here even if we to a merge before
         repo.getCommonService().saveOrUpdate(mergedBean);
         session.flush();
         commitTransction();
