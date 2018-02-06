74ad1146c0bdf678ccf849c3a76ce75218fec0e4
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2018-02-06 15:12:40 +0100

    documenting intendet usage of EntityEditorActionListener

--- src/main/java/eu/etaxonomy/vaadin/event/EntityEditorActionListener.java
+++ src/main/java/eu/etaxonomy/vaadin/event/EntityEditorActionListener.java
@@ -9,12 +9,33 @@
 package eu.etaxonomy.vaadin.event;
 
 /**
+ * Implementations will usually look like:
+ *
+ * <pre>
+    myComboboxSelect.setEditActionListener(e -> {
+
+            Object fieldValue = e.getSource().getValue();
+            Integer beanId = null;
+            if(fieldValue != null){
+                beanId = ((CdmBase)fieldValue).getId();
+
+            }
+            getViewEventBus().publish(this, new SomeEditorAction(e.getAction(), beanId, e.getSource(), this));
+        });
+  }
+ *</pre>
  * @author a.kohlbecker
  * @since Jan 17, 2018
  *
  */
 public interface EntityEditorActionListener {
 
+
+    /**
+     * see type level documentation for implementation hints.
+     *
+     * @param action
+     */
     public void onEntityEditorActionEvent(EntityEditorActionEvent action);
 
 }
