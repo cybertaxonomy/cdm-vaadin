5ff2ddc54ddf9a4aa0367040c11147ba7e9c4182
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2018-11-14 17:59:17 +0100

    adding user information to PopupEditorException messages

--- src/main/java/eu/etaxonomy/vaadin/mvp/PopupEditorException.java
+++ src/main/java/eu/etaxonomy/vaadin/mvp/PopupEditorException.java
@@ -8,6 +8,8 @@
 */
 package eu.etaxonomy.vaadin.mvp;
 
+import eu.etaxonomy.cdm.api.utility.UserHelper;
+import eu.etaxonomy.cdm.service.UserHelperAccess;
 import eu.etaxonomy.cdm.vaadin.event.EditorActionContextFormat;
 import eu.etaxonomy.cdm.vaadin.event.EditorActionContextFormat.TargetInfoType;
 import eu.etaxonomy.cdm.vaadin.event.EditorActionContextFormatter;
@@ -23,6 +25,8 @@ public class PopupEditorException extends RuntimeException {
 
     private String contextInfo;
 
+    private String userName;
+
     /**
      * @param message
      */
@@ -34,6 +38,15 @@ public class PopupEditorException extends RuntimeException {
         format.tagName = null;
         format.doTargetEntity = true;
         contextInfo = formatter.format(editor.getEditorActionContext(), format);
+        userName = userName();
+    }
+
+    /**
+     * @return
+     */
+    public String userName() {
+        UserHelper userHelper = UserHelperAccess.userHelper();
+        return userHelper != null ? userHelper.userName() : null;
     }
 
     public PopupEditorException(String message, AbstractPopupEditor<?,?> editor) {
@@ -44,11 +57,18 @@ public class PopupEditorException extends RuntimeException {
         format.tagName = null;
         format.doTargetEntity = true;
         contextInfo = formatter.format(editor.getEditorActionContext(), format);
+        userName = userName();
     }
+
+
 
     @Override
     public String getMessage() {
-        return super.getMessage() + " - context:" + contextInfo;
+        String userInfo = "";
+        if(userName != null){
+            userInfo = " [user=" + userName + "] ";
+        }
+        return super.getMessage() + " -  context" + userInfo + ":" + contextInfo;
     }
 
 
