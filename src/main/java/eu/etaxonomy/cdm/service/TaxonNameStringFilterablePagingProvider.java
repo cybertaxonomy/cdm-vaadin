eb8d2f54ceae4c3fa335a52607f76aa4bac7c4cc
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2019-03-13 09:02:09 +0100

    ref #8176 bugfix in TaxonNameStringFilterablePagingProvider.idFor()

--- src/main/java/eu/etaxonomy/cdm/service/TaxonNameStringFilterablePagingProvider.java
+++ src/main/java/eu/etaxonomy/cdm/service/TaxonNameStringFilterablePagingProvider.java
@@ -13,7 +13,6 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.UUID;
 
 import org.apache.log4j.Logger;
@@ -233,7 +232,7 @@ public class TaxonNameStringFilterablePagingProvider implements FilterableString
      */
     @Override
     public UUID idFor(String stringRepresentation) {
-        if(lastPagedEntityUUIDs == null){
+        if(lastPagedEntityUUIDs == null || !lastPagedEntityUUIDs.containsKey(stringRepresentation)){
             int pages = Math.max(1, size(stringRepresentation));
             for(int i = 0; i < pages; i++){
                 findEntities(i, stringRepresentation);
