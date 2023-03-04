8676938f1618c4a936739d986136449797e97436
Andreas Müller <a.mueller@bgbm.org>
2023-03-04 11:11:25 +0100

    try fix compile error (cont.)

--- src/main/java/eu/etaxonomy/vaadin/mvp/ApplicationView.java
+++ src/main/java/eu/etaxonomy/vaadin/mvp/ApplicationView.java
@@ -4,6 +4,6 @@ package eu.etaxonomy.vaadin.mvp;
  * An interface to be implemented by the specific view interface definitions.
  * See https://dev.e-taxonomy.eu/redmine/projects/edit/wiki/VaadinNotes#Model-View-Presenter-MVP-Pattern
  */
-public interface ApplicationView<P extends AbstractPresenter<?>> {
+public interface ApplicationView<P extends AbstractPresenter> {
 
 }
